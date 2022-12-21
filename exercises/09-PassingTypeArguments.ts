d6d0ec4b8cbf3b390dd641af7513e5d24c713c5c
Alan Ksenzov <fkdrsp@proton.me>
2022-12-21 16:58:36 +0100

    solved 09

--- exercises/09-PassingTypeArguments.ts
+++ exercises/09-PassingTypeArguments.ts
@@ -1,7 +1,10 @@
-const guitarists = new Set();
+const guitarists = new Set<string>();
 
 guitarists.add("Jimi Hendrix");
 guitarists.add("Eric Clapton");
+
+// This now throws an error
+// guitarists.add(2);
 
 // This set is supposed to only consist of strings
 // How would you achieve this?
