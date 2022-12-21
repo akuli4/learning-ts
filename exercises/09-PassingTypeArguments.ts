02c104aad81476e0a5616e1f4c83faccdb7d143d
Alan Ksenzov <fkdrsp@proton.me>
2022-12-21 16:58:04 +0100

    introduced to type args

---
+++ exercises/09-PassingTypeArguments.ts
@@ -0,0 +1,7 @@
+const guitarists = new Set();
+
+guitarists.add("Jimi Hendrix");
+guitarists.add("Eric Clapton");
+
+// This set is supposed to only consist of strings
+// How would you achieve this?
