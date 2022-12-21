50a2be6cef2c2aeb2fb9d04a1f43e40199360dcf
Alan Ksenzov <fkdrsp@proton.me>
2022-12-21 17:18:25 +0100

    solved 11

--- exercises/11-NarrowingDownUnions.ts
+++ exercises/11-NarrowingDownUnions.ts
@@ -1,7 +1,12 @@
-const coerceAmount = (amount: number | { amount: number }) => {};
+const coerceAmount = (amount: number | { amount: number }) => {
+  if (typeof amount === "number") {
+    return amount;
+  }
+  return amount.amount;
+};
 
 // "Should return the amount when passed an object"
 // "Should return the amount when passed a number",
 
 const a = coerceAmount(20);
-// a === 20 - FAILS
+const b = coerceAmount({ amount: 20 });
