f6ecf06b8e100f0ba5e6ceeff4b7c2cfd00da6fc
Alan Ksenzov <fkdrsp@proton.me>
2022-12-21 19:33:07 +0100

    introduced async type problem

---
+++ exercises/17-typingAsyncFunctions.ts
@@ -0,0 +1,16 @@
+interface User {
+	id: string;
+	firstName: string;
+	lastName: string;
+}
+
+const createThenGetUser = async (
+	createUser: unknown,
+	getUser: unknown
+): Promise<User> => {
+	const userId: string = await createUser();
+
+	const user = await getUser(userId);
+
+	return user;
+};
