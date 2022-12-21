237b348f6b7e7a159354f0c4b59629016ca137ce
Alan Ksenzov <fkdrsp@proton.me>
2022-12-21 16:06:33 +0100

    solved 04

--- exercises/04-union.ts
+++ exercises/04-union.ts
@@ -3,13 +3,12 @@ interface User {
   firstName: string;
   lastName: string;
   //   I need a selection of roles that i can choose from.
-  role: string;
+  role: "user" | "editor" | "admin";
 }
 
 export const defaultUser: User = {
   id: 1,
   firstName: "Matt",
   lastName: "Pocock",
-  // @ts-expect-error
-  role: "I_SHOULD_NOT_BE_ALLOWED",
+  role: "admin",
 };
