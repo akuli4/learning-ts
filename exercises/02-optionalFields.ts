5c629409e6079a43b733df758fe799008d59afa2
Alan Ksenzov <fkdrsp@proton.me>
2022-12-21 15:25:27 +0100

    solved 02

--- exercises/02-optionalFields.ts
+++ exercises/02-optionalFields.ts
@@ -1,4 +1,4 @@
-export const getName = (params: { first: string; last: string }) => {
+export const getName = (params: { first: string; last?: string }) => {
   if (params.last) {
     return `${params.first} ${params.last}`;
   }
