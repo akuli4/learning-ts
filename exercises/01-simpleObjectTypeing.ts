b27b382ea2964098fb59f057a50b8ecd65ceb425
Alan Ksenzov <fkdrsp@proton.me>
2022-12-21 15:14:35 +0100

    solved 01

--- exercises/01-simpleObjectTypeing.ts
+++ exercises/01-simpleObjectTypeing.ts
@@ -1,3 +1,19 @@
-export const addTwoNumbers = (params) => {
+export const addTwoNumbers = (params: { first: number; second: number }) => {
   return params.first + params.second;
+};
+
+// OR
+interface IParams {
+  first: number;
+  second: number;
+}
+
+export const addTwoNumbers1 = (params: IParams) => {
+  return params.first + params.second;
+};
+
+// OR use Types, but Interfaces are better suited for objects.
+type TParams = {
+  first: number;
+  second: number;
 };
