be967374972290253c493e86cdeb00b4bf800e91
Alan Ksenzov <fkdrsp@proton.me>
2022-12-21 18:05:53 +0100

    solved 15 with two solutions

--- exercises/15-constructTypesFromOtherTypes.ts
+++ exercises/15-constructTypesFromOtherTypes.ts
@@ -1,7 +1,22 @@
 interface IUser {
 	id: number;
+	firstName: string;
 	lastName: string;
-	secondName: string;
 }
 
 // Construct a new type containing only first- and last names
+
+// #1
+type FirstSolution = Pick<IUser, "firstName" | "lastName">;
+
+// #2
+type SecondSolution = Omit<IUser, "id">;
+
+const a: FirstSolution = {
+	firstName: "First",
+	lastName: "Last",
+};
+const b: SecondSolution = {
+	firstName: "First",
+	lastName: "Last",
+};
