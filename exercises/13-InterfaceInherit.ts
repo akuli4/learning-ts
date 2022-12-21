b7e14efa70c57c8ed118d8c2e1f5643d1e40cf36
Alan Ksenzov <fkdrsp@proton.me>
2022-12-21 17:39:01 +0100

    introduced to inheritance problem

---
+++ exercises/13-InterfaceInherit.ts
@@ -0,0 +1,18 @@
+// Extend every interface to use recurring fields
+
+interface User {
+  id: string;
+  firstName: string;
+  lastName: string;
+}
+
+interface Post {
+  id: string;
+  title: string;
+  body: string;
+}
+
+interface Comment {
+  id: string;
+  comment: string;
+}
