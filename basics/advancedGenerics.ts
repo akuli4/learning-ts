7bb4385296ac02a506e7a5d18dea0b3441f7063b
Alan Ksenzov <fkdrsp@proton.me>
2022-12-22 22:36:19 +0100

    how to tell typescript what this TItem is?

--- basics/advancedGenerics.ts
+++ basics/advancedGenerics.ts
@@ -2,8 +2,15 @@
 		Ranker takes in a value and a rank function.
 	Make so that this function uses generic typing.
 */
+interface RankedItems {
+	item: TItem;
+	rank: number;
+}
 
-const ranker = (items, rank) => {
+const ranker = <TItem>(
+	items: Array<TItem>,
+	rank: (v: TItem) => number
+): TItem[] => {
 	const ranks = items.map((item) => ({
 		item,
 		rank: rank(item),
