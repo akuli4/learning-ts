c42aa863f263ca22cd11f25e59a0d2ab02a05e10
Alan Ksenzov <fkdrsp@proton.me>
2022-12-23 22:01:15 +0100

    shown example of otside constructor decalration

--- classes/readonlyStatic.ts
+++ classes/readonlyStatic.ts
@@ -7,5 +7,12 @@ class Dog {
 	constructor(public readonly name: string, public readonly age: number) {}
 }
 
-const Tanuki = new Dog("Tanuki", 20);
+class Dolmatain extends Dog {
+	public readonly breed = "Dolmatain";
+	constructor(public readonly name: string, public readonly age: number) {
+		super(name, age);
+	}
+}
+
+const Tanuki = new Dolmatain("Tanuki", 20);
 console.log(Tanuki); // Dog { name: "Tanuki", age: 20 }
