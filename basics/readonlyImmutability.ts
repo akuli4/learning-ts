e1eb6f5b65c7ff3bd0c756bf2439491fedeb3ec5
Alan Ksenzov <fkdrsp@proton.me>
2022-12-23 14:57:37 +0100

    solved readonly problem

--- basics/readonlyImmutability.ts
+++ basics/readonlyImmutability.ts
@@ -2,13 +2,14 @@
 		Make cat readonly!!
 	I dont want random people to rename my cat :(
 */
-
+// 	Read only can be placed on a separate
+// fields or we can make whole type be readonly.
 interface Cat {
 	name: string;
 	breed: string;
 }
 
-const makeCat = (name: string, breed: string): Cat => {
+const makeCat = (name: string, breed: string): Readonly<Cat> => {
 	return {
 		name,
 		breed,
