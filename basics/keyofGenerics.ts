fb4a97f0ab1efd052b446ae902d226dbbba3ce7b
Alan Ksenzov <fkdrsp@proton.me>
2022-12-23 13:08:43 +0100

    solved keyof basic problem

--- basics/keyofGenerics.ts
+++ basics/keyofGenerics.ts
@@ -5,6 +5,28 @@
 	Type this function using keyof.
 */
 
-function pluck(items: unknown[], key: string): unknown[] {
+function pluck<T, K extends keyof T>(items: T[], key: K): T[K][] {
 	return items.map((item) => item[key]);
 }
+
+interface Animal {
+	name: string;
+	age: number;
+}
+
+const animals: Array<Animal> = [
+	{
+		name: "Zebra",
+		age: 12,
+	},
+	{
+		name: "Giraffe",
+		age: 10,
+	},
+	{
+		name: "Cat",
+		age: 3,
+	},
+];
+
+console.log(pluck(animals, "name"));
