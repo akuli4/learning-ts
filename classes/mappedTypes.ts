e806b593cb215cce6e38998e958b2779b2f83e1b
Alan Ksenzov <fkdrsp@proton.me>
2022-12-23 19:45:15 +0100

    solved more complex type mapping problem

--- classes/mappedTypes.ts
+++ classes/mappedTypes.ts
@@ -7,3 +7,14 @@ const doggo: Dog = {
 	name: "Tanuki",
 	age: 2,
 };
+
+interface IDogInfo {
+	name: string;
+	age: number;
+}
+// T = IDogInfo, keyof T are its keys, P in = forEach
+type DogOptionasFlags<T> = {
+	[P in keyof T]: string;
+};
+
+type BoolDog = DogOptionasFlags<IDogInfo>;
