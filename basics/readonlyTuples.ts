9e931aceb68a3ce28c3eed23314fef79307628d1
Alan Ksenzov <fkdrsp@proton.me>
2022-12-23 15:02:48 +0100

    introduced to readonly tuples problem

---
+++ basics/readonlyTuples.ts
@@ -0,0 +1,11 @@
+const getCursorCoordinates = (): [number, number, number] => {
+	return [
+		Number((Math.random() * 10).toFixed()), // X
+		Number((Math.random() * 10).toFixed()), // Y
+		Number((Math.random() * 10).toFixed()), // Z
+	];
+};
+
+const cursor = getCursorCoordinates();
+
+cursor[0] = 20; // This should not be possible
