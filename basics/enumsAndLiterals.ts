9857c1bdd9101abf2baa3c328874c5908ad2bf85
Alan Ksenzov <fkdrsp@proton.me>
2022-12-23 15:29:12 +0100

    solved literals types problem

--- basics/enumsAndLiterals.ts
+++ basics/enumsAndLiterals.ts
@@ -16,3 +16,15 @@ const isLoading = (state: Loading): boolean => {
 };
 
 console.log(isLoading(Loading.loading));
+
+// Literals
+const rollDice = (dice: 1 | 2 | 3): number => {
+	let pip = 0;
+	for (let i = 0; i < dice; i++) {
+		pip += Number((Math.random() * 5 + 1).toFixed());
+	}
+	return pip;
+};
+
+// Not allowed
+console.log(rollDice(5));
